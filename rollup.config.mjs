250d5a4504efaf7545bb8960cd0574d74a218e3f
nischit <nischitpra@gmail.com>
2024-05-05 02:57:31 +0530

    remove minification

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
 import json from "@rollup/plugin-json";
-import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "src/index.js",
@@ -22,7 +21,6 @@ export default {
       presets: ["@babel/preset-react"],
     }),
     commonjs(),
-    terser(), // Minifies the output
   ],
   external: ["react", "react-dom", "@walletconnect/ethereum-provider", "@walletconnect/modal", "ethers"],
 };
